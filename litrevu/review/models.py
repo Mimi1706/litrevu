47239c509f0d03aef282575760a06be04062568e
Mimi <hana.nguyen@outlook.fr>
2023-09-12 23:23:25 +0200

    Splitting review and ticket

--- litrevu/review/models.py
+++ litrevu/review/models.py
@@ -1,16 +1,7 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
-
-
-class Ticket(models.Model):
-    # Your Ticket model definition goes here
-    title = models.CharField(max_length=128)
-    description = models.TextField(max_length=2048, blank=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
-    image = models.ImageField(null=True, blank=True)
-    time_created = models.DateTimeField(auto_now_add=True)
+from ticket.models import Ticket
 
 
 class Review(models.Model):
